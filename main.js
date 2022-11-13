2b510d271aa3c52b1c2b249eaae0cac78d320dc8
SDCored <sdcored@gmail.com>
2022-11-12 23:38:04 -0600

    Small Updates

--- main.js
+++ main.js
@@ -15,15 +15,13 @@ async function tweetScore() {
 	(function loop() {
 		var now = new Date();
 
-		if (now.getMinutes() === 00) {
+		if (now.getMinutes() === 00 && now.getSeconds() < 30) {
 			axios
 				.get("https://api.apexstats.dev/minPred")
 				.then(function (response) {
 					const data = response.data;
 
-					const tweetText = `-RP/AP Threshold for Apex Predator-\n\nPlatform: BR / Arenas\nPC: ${data.PC.Battle_Royale.toLocaleString()} RP / ${data.PC.Arenas.toLocaleString()} AP\nPlayStation: ${data.PS4.Battle_Royale.toLocaleString()} RP / ${data.PS4.Arenas.toLocaleString()} AP\nXbox: ${data.X1.Battle_Royale.toLocaleString()} RP / ${data.X1.Arenas.toLocaleString()} AP\nSwitch: ${data.Switch.Battle_Royale.toLocaleString()} RP / ${data.Switch.Arenas.toLocaleString()} AP\n\n#ApexLegends\nLast Updated: ${Math.floor(
-						new Date().getTime() / 1000
-					)}`;
+					const tweetText = `-RP/AP Threshold for Apex Predator-\n\nPlatform: BR -- Arenas\nPC: ${data.PC.Battle_Royale.toLocaleString()} RP -- ${data.PC.Arenas.toLocaleString()} AP\nPlayStation: ${data.PS4.Battle_Royale.toLocaleString()} RP -- ${data.PS4.Arenas.toLocaleString()} AP\nXbox: ${data.X1.Battle_Royale.toLocaleString()} RP -- ${data.X1.Arenas.toLocaleString()} AP\nSwitch: ${data.Switch.Battle_Royale.toLocaleString()} RP -- ${data.Switch.Arenas.toLocaleString()} AP\n\n#ApexLegends #ApexLegendsRanked`;
 
 					const tweet = () => {
 						const onFinish = (err, reply) => {
